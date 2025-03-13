cfc32b491e6d39f76a38fba5f841cee7c2d35758
ItsMee04 <meemeeits@gmail.com>
2025-03-13 14:13:45 +0700

    First Commit

--- public/assets/js/produksi.js
+++ public/assets/js/produksi.js
@@ -922,13 +922,42 @@ $(document).ready(function () {
                 // Tampilkan modal edit
                 $("#mdEditUpdateProgres").modal("show");
             },
-            error: function () {
-                Swal.fire(
-                    "Gagal!",
-                    "Tidak dapat mengambil data Progres Bangunan.",
-                    "error"
-                );
-            },
+            error: function (xhr) {
+                var Toast = Swal.mixin({
+                    toast: true,
+                    position: "top-end",
+                    showConfirmButton: false,
+                    timer: 3000,
+                });
+
+                if (xhr.responseJSON && xhr.responseJSON.errors) {
+                    const errors = xhr.responseJSON.errors;
+                    let errorMessage = "";
+
+                    for (let key in errors) {
+                        if (errors.hasOwnProperty(key)) {
+                            errorMessage += `${errors[key][0]}\n`; // Ambil pesan pertama dari setiap error
+                        }
+                    }
+
+                    Toast.fire({
+                        icon: "error",
+                        title: errorMessage.trim(),
+                    });
+
+                } else if (xhr.responseJSON && xhr.responseJSON.message) {
+                    Toast.fire({
+                        icon: "error",
+                        title: xhr.responseJSON.message,
+                    });
+
+                } else {
+                    Toast.fire({
+                        icon: "error",
+                        title: "Terjadi kesalahan. Silakan coba lagi!",
+                    });
+                }
+            }
         });
     });
 
